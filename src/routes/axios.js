482af0cd0d2109d32e1821c4777820a8d07d6b35
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-23 10:20:53 +0530

    Update axios.js

--- src/routes/axios.js
+++ src/routes/axios.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 const accessToken = token ? token : null; // No need to parse as JSON
 
 const instance = axios.create({
-  baseURL: 'https://elarning-backend-1-wkvk.onrender.com',
+  baseURL: 'https://elarning-backend-1-wkvk.onrender.com/api',
   headers: {
     'Authorization': `Bearer ${accessToken}`,
     'Content-Type': "application/json", 
