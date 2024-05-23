fb60f6d70f15b999aadd87a9e6649ba74098c3c7
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-23 10:58:31 +0530

    Update nonauthaxios.js

--- src/routes/nonauthaxios.js
+++ src/routes/nonauthaxios.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
 
+
+
+const axiosinstance = axios.create({
+  baseURL: 'https://elarning-backend-1-wkvk.onrender.com/api',
+  headers: {
+    'Content-Type': "application/json", 
+  }, 
+});
+
+export default axiosinstance;
