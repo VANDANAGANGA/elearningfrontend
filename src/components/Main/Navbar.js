79d313c577243a188da7bc2e3fc7fd78cb772793
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-07-11 14:07:58 +0530

    Update Navbar.js

--- src/components/Main/Navbar.js
+++ src/components/Main/Navbar.js
@@ -2,17 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Avatar from 'react-avatar';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-<<<<<<< HEAD
-import { useEffect } from 'react';
-=======
->>>>>>> 532dd73d11bdb86b1b550517d541443b0ac3dc8e
+
 import instance from '../../routes/axios';
 import { baseUrl } from '../../utils/urls';
 import { deleteUser } from '../../Store/authSlice';
 import { useDispatch } from 'react-redux';
 
 function Navbar() {
-<<<<<<< HEAD
   const dispatch = useDispatch();
   const navigate=useNavigate()
   const [profilePic,setProfilePic]=useState()
@@ -36,29 +32,13 @@ function Navbar() {
   };
 
 
-   useEffect(() => {
-=======
-  const navigate = useNavigate();
-  const [profilePic, setProfilePic] = useState(null);
-  const user = useSelector((state) => state.authUser.user); // Accessing user details from Redux store
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  };
-
+ 
   useEffect(() => {
->>>>>>> 532dd73d11bdb86b1b550517d541443b0ac3dc8e
     const fetchProfilePic = async () => {
       if (!user?.id) return;
-
       try {
         const response = await instance.get('profilepic/', {
-<<<<<<< HEAD
-          params: { id: user?.id },
-=======
           params: { id: user.id },
->>>>>>> 532dd73d11bdb86b1b550517d541443b0ac3dc8e
         });
         setProfilePic(response.data.profile_pic);
       } catch (error) {
@@ -67,16 +47,12 @@ function Navbar() {
     };
 
     fetchProfilePic();
-  }, [user]); // Correctly set the dependency array
+  }, [user]); 
 
   return (
     <header className="mt-4">
       <nav className="flex justify-between items-center bg-white bg-opacity-20 h-[85px] px-8">
         <div>
-<<<<<<< HEAD
-            <h1 style={{ margin: '-1', padding: '-1' }}  className="text-gray-500 font-[900] text-[35px] my-0 py-0">SKILLSAGA</h1>
-            <span style={{ margin: '-1', padding: '-1' }}  className="text-gray-500 text-[18px] my-0 py-0 ">ONLINE EDUCATION & LEARNING</span>
-=======
           <h1 style={{ margin: '-1', padding: '-1' }} className="text-gray-500 font-[900] text-[35px] my-0 py-0">
             SKILLSAGA
           </h1>
@@ -87,7 +63,6 @@ function Navbar() {
         <div className="flex items-center justify-center h-[85px] w-[300px]">
           <div className="pr-4">
             <h4>Welcome {user?.name?.toUpperCase()}</h4>
->>>>>>> 532dd73d11bdb86b1b550517d541443b0ac3dc8e
           </div>
           <div>
             <Avatar size="60" round={true} src={`${baseUrl}${profilePic}`} />
